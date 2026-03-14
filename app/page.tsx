06ccae7f37af62ab1bd5ec2f9b95df65cd2565df
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-14 22:18:38 +0530

    Add some UI for tesing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+  console.log("SESSION:: ", session);
+  
+  if (!session) {
+    return <div> Not login... </div>
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button>Click me</Button>
-    </div>
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+          <div>
+              <h1>Welcome {session.user.name}</h1>
+          </div>
+      </div>
   );
 }
