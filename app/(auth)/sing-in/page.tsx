88acd907b41d3be8bbeab05201d13314c14a02f6
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-14 22:20:04 +0530

    Add sing In Page

--- app/(auth)/sing-in/page.tsx
+++ app/(auth)/sing-in/page.tsx
@@ -1,9 +1,14 @@
+import SingInForm from '@/components/signin-form';
 import React from 'react'
 
 const SingInPage = () => {
   return (
-    <div>SingInPage</div>
-  )
+      <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+          <div className="w-full max-w-sm">
+              <SingInForm />
+          </div>
+      </div>
+  );
 }
 
 export default SingInPage;
