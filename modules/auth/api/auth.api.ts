b7aa89ebf057d6d300a8bb28365f8c546d757e81
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-19 17:40:34 +0530

    Make modules base structure

---
+++ modules/auth/api/auth.api.ts
@@ -0,0 +1,49 @@
+import { SignInUserData, SignUpUserData } from "@/types";
+import { signInSchema, signUpSchema } from "@/lib/validators/auth.validator";
+
+export const signInUser = async (data: SignInUserData) => {
+    const validated = signInSchema.safeParse(data);
+    if (!validated.success) return {
+        success: false,
+        message: validated.error.issues.map((e) => e.message).join(", ")
+    }
+    const res = await fetch("/api/auth/sign-in/email", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            ...validated.data,
+            rememberMe: true,
+            callbackURL: window.location.origin,
+        }),
+    });
+
+    if (!res.ok) {
+        throw new Error("Login failed");
+    }
+
+    return res.json();
+};
+
+export const signUpUser = async (data: SignUpUserData) => {
+    const validated = signUpSchema.safeParse(data);
+    if (!validated.success) return {
+        success: false,
+        message: validated.error.issues.map((e) => e.message).join(", "),
+    };
+
+    const res = await fetch("/api/auth/sign-up/email", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            ...validated.data,
+            callbackURL: window.location.origin,
+        }),
+    });
+
+    if (!res.ok) throw new Error("Signup failed");
+    return res.json();
+};
