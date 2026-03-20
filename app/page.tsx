e03fe565ca008d63ada4fac73751d275a2a95c07
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-20 20:09:44 +0530

    update UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,8 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+import HOME from "@/components/home-com";
+
 
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  });
-  console.log("SESSION:: ", session);
-  
-  if (!session) {
-    return <div> Not login... </div>
-  }
-
-  return (
-      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-          <div>
-              <h1>Welcome {session.user.name}</h1>
-          </div>
-      </div>
-  );
+    return <div>
+      <HOME />
+    </div>;
 }
