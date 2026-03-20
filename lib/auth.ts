10179c447a28606e949fcffa30ea07d39c93e291
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-20 19:57:27 +0530

    add next cokkies plugin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "./db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
     database: prismaAdapter(db, {
@@ -20,7 +21,5 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }
     },
-    trustedOrigins: [
-        "http://localhost:3000",
-    ],
+    plugins: [nextCookies()]
 });
