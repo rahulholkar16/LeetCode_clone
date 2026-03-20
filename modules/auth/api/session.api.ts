77e8b209f67dd1cb8b292e5d5f3d6ab8c1fd1995
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-20 19:24:39 +0530

    Ad session api and fix auth error

---
+++ modules/auth/api/session.api.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export const fetchSession = async () => {
+    try {
+        const session = await auth.api.getSession({
+            headers: await headers(),
+        });
+
+        return session;
+    } catch (error) {
+        console.error("SESSION ERROR:: ", error);
+        return null;
+    }
+};
