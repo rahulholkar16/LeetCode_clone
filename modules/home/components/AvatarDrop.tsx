c98d54d4cc0184a011974c89d14d86e1fa3094fc
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-21 19:26:44 +0530

    Add User Avatar dropdown

---
+++ modules/home/components/AvatarDrop.tsx
@@ -0,0 +1,55 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/modules/auth/hooks/useAuth";
+import { AvatarDropdownProp } from "@/types";
+
+export function AvatarDropdown({ user }: AvatarDropdownProp) {
+    const { signOut } = useAuth();
+    async function handleLogOut () {
+        await signOut();
+    };
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="rounded-full">
+                    <Avatar>
+                        {user?.image && (
+                            <AvatarImage
+                                src={user.image}
+                                alt={user.name || "User"}
+                            />
+                        )}
+
+                        <AvatarFallback className="bg-linear-to-br from-yellow-400 to-orange-500 text-black font-bold">
+                            {user?.name?.charAt(0).toUpperCase() || "U"}
+                        </AvatarFallback>
+                    </Avatar>
+                </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent className="w-40">
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Settings</DropdownMenuItem>
+                </DropdownMenuGroup>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuGroup>
+                    <DropdownMenuItem className="text-red-500 focus:text-red-500" onClick={handleLogOut}>
+                        Log out
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
