fede224e206672be598955bdb60e163f1a04e4f2
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-21 23:42:22 +0530

    Add HOME UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
-import HOME from "@/components/home-com";
+import { CategoriesSection } from "@/modules/home/components/CategoriesSection";
+import { CTASection } from "@/modules/home/components/CTASection";
+import { FeaturesGrid } from "@/modules/home/components/FeaturesGrid";
+import { HeroSection } from "@/modules/home/components/HeroSection";
 
-export default async function Home() {
-    return <div>
-      <HOME />
-    </div>;
+export default function Home() {
+  return (
+      <div className="min-h-screen">
+          <HeroSection />
+          <FeaturesGrid />
+          <CategoriesSection />
+          <CTASection />
+      </div>
+  );
 }
