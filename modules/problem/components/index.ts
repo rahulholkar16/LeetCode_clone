2a1ededd5c663318c98bc8864a66b89b1a7bc8e7
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-25 20:45:21 +0530

    Add importing and exporting file

---
+++ modules/problem/components/index.ts
@@ -0,0 +1,6 @@
+export { BasicInformation } from './BasicInformation';
+export { ProblemDescription } from './ProblemDescription';
+export { ExamplesSection } from './ExamplesSection';
+export { TestCasesSection } from './TestCasesSection';
+export { CodeSnippetsSection } from './CodeSnippetsSection';
+export { ReferenceSolutionSection } from './ReferenceSolutionSection';
