d4c95d930dfe29ffdd0b8b74c976c359601be0a4
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-28 20:52:28 +0530

    update problem NULL to empty arry

--- modules/problem/stores/problem-store.ts
+++ modules/problem/stores/problem-store.ts
@@ -2,7 +2,7 @@ import { ProblmStore } from "@/types";
 import { create } from "zustand";
 
 export const useProblmStore = create<ProblmStore>((set) => ({
-    problems: null,
+    problems: [],
     setProblems: (problems) => set({ problems }),
     setProblem: (problem) => {
         set((state) => ({
