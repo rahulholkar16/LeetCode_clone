ae37b5dff8c8e683c9830adc83d7042a977588ae
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-12 13:26:32 +0530

    Add Spinner in loading

--- modules/auth/components/auth-layer.tsx
+++ modules/auth/components/auth-layer.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import { ChildrenProps } from '@/types';
-import { useSession } from '../hooks/useSession'
+import { ChildrenProps } from "@/types";
+import { useSession } from "../hooks/useSession";
+import { Spinner } from "@/components/ui/spinner";
 
 const AuthLayer = ({ children }: ChildrenProps) => {
-    const {isLoading} = useSession();
+    const { isLoading } = useSession();
 
-    if (isLoading) return <div className='text-center'>Loading...</div>
+    if (isLoading)
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <Spinner />
+            </div>
+        );
 
-    return (
-        <>{children}</>
-    )
-}
+    return <>{children}</>;
+};
 
 export default AuthLayer;
