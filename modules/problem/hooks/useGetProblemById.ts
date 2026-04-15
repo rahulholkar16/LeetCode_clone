e5371425fc732c23d6ff4e54cbea9abf375a0c73
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-15 19:23:28 +0530

    fix set problem in zustand store

--- modules/problem/hooks/useGetProblemById.ts
+++ modules/problem/hooks/useGetProblemById.ts
@@ -1,24 +1,21 @@
-"use client";
-
-import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { useProblmStore } from "../stores/problem-store";
 import { getProblemById } from "../actions/problem.action";
+import { useProblmStore } from "../stores/problem-store";
 
 export const useGetProblemById = (id: string) => {
     const addProblem = useProblmStore((state) => state.addProblem);
 
     const query = useQuery({
         queryKey: ["problem", id],
-        queryFn: () => getProblemById(id),
+        queryFn: async () => {
+            const result = await getProblemById(id);
+            if (result.success && result.data) {
+                addProblem(result.data);
+            }
+            return result;
+        },
         staleTime: 1000 * 60 * 5,
     });
-
-    useEffect(() => {
-        if (query.data?.success && query.data.data) {
-            addProblem(query.data.data);
-        }
-    }, [query.data, addProblem]);
 
     return query;
 };
